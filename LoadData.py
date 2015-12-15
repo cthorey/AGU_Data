66fe4db87328ef4b5a847fd169e62486a0e7a285
cthorey <clement.thorey@gmail.com>
2015-12-14 17:15:30 -0800

    update output

--- LoadData.py
+++ LoadData.py
@@ -3,12 +3,13 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium import webdriver
-import pickle,time
+import pickle,time,datetime
 from bs4 import BeautifulSoup
 from tqdm import *
 
-#racine = '/Users/thorey/Documents/MLearning/Side_Project/AGU_Data/'
-racine = '/Users/clement/AGU_Data' 
+racine = '/Users/thorey/Documents/MLearning/Side_Project/AGU_Data/'
+#racine = '/Users/clement/AGU_Data' 
+
 class Paper(object):
     ''' Class to handle each paper on the website AGU'''
     def __init__(self,body1,body2,body3,body4,idx):
@@ -31,12 +32,13 @@ def Scrapper(start,end,base_url):
     papers = []
     errors = []
     first,last = start,end
-    for idx in tqdm(range(start,end,2)):
+    progress = open(os.path.join(racine,'progress.txt'),'w+')
+    for idx in tqdm(range(start,end,2),file = progress):
         idx = str(idx)
         link = os.path.join(base_url,idx)
         wd.get(link)
         # Wait for the dynamically loaded elements to show up
-        timeout = 30
+        timeout = 20
         try:
             WebDriverWait(wd, timeout).until(
                 EC.visibility_of_element_located((By.XPATH,'//*[@id="Content"]/section[1]')))  
@@ -64,7 +66,8 @@ def Scrapper(start,end,base_url):
         pickle.dump({'papers':papers,'error':errors}, fi, pickle.HIGHEST_PROTOCOL)
     
     wd.quit()
-
+    progress.close()
+    
 def calc_end(end,base_end):
     if end > base_end:
         return base_end
@@ -87,16 +90,20 @@ base_start = 58180
 base_end = 87000
 
 #What remains to do
-step = 50 # Diviser par 2 pour avoir le nombre de paper a download
+step = 25 # Diviser par 2 pour avoir le nombre de paper a download
 start = calc_start(base_start)
 end = calc_end(start+step,base_end)
 
-print 'We take back from paper %d'%(start)
+bilan = open(os.path.join(racine,'Bilan.txt'),'a')
+bilan.write('hello \n')
+bilan.write('Scrapping commencer le %s \n'%(str(datetime.date.today())))
+bilan.write('We take back from paper %d \n'%(start))
 bool_end = True
 while bool_end:
     Scrapper(start,end,base_url)
-    print 'Succesfully donwload papers from %d to %d'%(start,end)
+    bilan.write('Succesfully donwload papers from %d to %d'%(start,end))
     start = end
     end = calc_end(start+step,base_end)
     if end == base_end:
         bool_end = False
+bilan.close()
